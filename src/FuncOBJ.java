4870972a13400df665c9f19238b86b74f4089f18
LinkHpp <pokD03@x.y.z>
2023-03-27 09:17:38 +0200

    FuncOBJ template parcial complete

---
+++ src/FuncOBJ.java
@@ -0,0 +1,48 @@
+
+public class FuncOBJ {
+
+    // Atributos
+
+    public enum Status {
+        Not_Finished, Paused, Incomplete, Complete
+    }
+
+    String NameFunc;
+    String Proyect;
+    String Language;
+
+    // Getters y Setters
+
+    public String getNameFunc() {
+        return NameFunc;
+    }
+
+    public void setNameFunc(String NameFunc) {
+        this.NameFunc = NameFunc;
+    }
+
+    public String getProyect() {
+        return Proyect;
+    }
+
+    public void setProyect(String proyect) {
+        this.Proyect = proyect;
+    }
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String Language) {
+        this.Language = Language;
+    }
+
+    // Constructor
+
+    public FuncOBJ(String nameFunc, String proyect, String language) {
+        NameFunc = nameFunc;
+        Proyect = proyect;
+        Language = language;
+    }
+
+}
