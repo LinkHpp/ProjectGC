890c5c7f0111e606b88d920b63904c55261901e3
Prakxo <lycos2003@gmail.com>
2023-03-27 09:34:43 +0200

    preliminary class for proyobj

---
+++ src/ProyOBJ.java
@@ -0,0 +1,51 @@
+public class ProyOBJ{
+
+    //Attr
+
+    String name;
+    int ver;
+
+    public enum Stat {
+        Unfinished, //0
+        Finished, //1
+        OnHold, //2
+    }
+
+    public Stat stat;
+
+    public enum Type {
+        Decomp, //Default, 0
+        Disasm, //1
+    }
+
+    public Type type;
+
+    //set/get
+    public String getname(void){
+
+        return name;
+    }
+
+    public int getver(void){
+        
+        return ver;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public void setver(int ver){
+        this.ver = ver;
+    }
+
+    //ctor
+
+    public ProyOBJ(String Name, int Ver){
+        name = Name;
+        ver = Ver;
+        stat = Stat.Unfinished;
+        type = Type.Decomp;
+    }
+
+}
